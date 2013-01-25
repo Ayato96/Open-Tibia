241223abe138ce55b9712c0217f5c8ccd16bafca
Bruno Lopes <brunolpz@live.com>
2013-01-25 18:27:56 -0200

    Added "Pet System OOP"

---
+++ pet system oop/data/lib/pet-species.lua
@@ -0,0 +1,3 @@
+PetSpecie:new("Rat", 20, 0, 5, 5, {[1] = Rock_Throw, [2] = Dark_Bite}, "Cave Rat", 14)
+PetSpecie:new("Cave Rat", 40, 20, 10, 10, {[1] = Dark_Bite}, "Munster", 32)
+PetSpecie:new("Munster", 100, 50, 20, 20, {[1] = Dark_Bite}, false, false)
